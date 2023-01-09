8396d4e1e4853fdc97b5ccd2ff5409d0d83b4c27
Dylan Curtis <Dcurtisj@gmail.com>
2023-01-08 19:01:54 -0800

    commit tree a407a8ef1a1a727dc02d3c89251b6abe40caaae1

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,26 @@
-#write your code here
-
-def countdown
+def countdown(int)
+if int.class == Integer
+    i = int
+    while i > 0
+        puts "#{i} SECOND(S)!"
+        i -= 1
+     end
+     return "HAPPY NEW YEAR!"    
+else
+    puts "not an integer"
 end
+end
+
+def countdown_with_sleep(int)
+    if int.class == Integer
+        i = int 
+        while i > 0            
+            puts "#{i} SECOND(S)!"
+            sleep 1
+            i -= 1
+         end
+         return "HAPPY NEW YEAR!"    
+    else
+        puts "not an integer"
+    end
+    end
